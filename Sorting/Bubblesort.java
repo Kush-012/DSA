b6c3929894869d917e09b846b2f2d6e55373e11a
Kush Mehta <149225289+Kush-012@users.noreply.github.com>
2026-01-06 13:56:02 +0530

    Create Bubblesort.java

---
+++ Sorting/Bubblesort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int n;
+      Scanner sc=new Scanner(System.in);
+      n=sc.nextInt();
+      int arr[]=new int[n];
+      for(int i=0;i<arr.length;i++){
+        arr[i]=sc.nextInt();
+      } 
+      for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+          if(arr[j+1]<arr[j]){
+            int a=arr[i];
+            arr[i]=arr[j];
+            arr[j]=a;
+          }
+        }
+      } 
+      
+      for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+      }
+      
+  }
+}  
